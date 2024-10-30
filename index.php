af2206c5fa49763b9d2d3e4a81fbf30a9cf48c7f
Jesus Almanza <crhistiko@gmail.com>
2024-10-30 11:58:51 -0400

    avance de la semana php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active" data-bs-interval="10000">
-            <img src="img/logo oficial.jpg" class="d-block w-100 " alt="logo">
+            <img src="img/logoOficial.jpg" class="d-block w-100 " alt="logo">
           </div>
           <div class="carousel-item" data-bs-interval="2000">
             <img src="img/Descargar fondos de pantalla por la mañana, lago, niebla, montañas, bosque, Alterschrofen, Baviera, Alemania libre_ Imágenes fondos de descarga gratuita.jpg" class="d-block w-100" alt="img">
@@ -49,7 +49,7 @@
       </div>
   
 
-
+      <?php require_once "footer.php"; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="app.js"></script>
 </body>
