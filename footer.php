993420983611cd938c5f812ba754e30b1a7e2914
Jesus Almanza <crhistiko@gmail.com>
2024-10-30 16:15:47 -0400

    Footer

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+<button class="button" data-text="Awesome">
+    <span class="actual-text">&nbsp;PROMAC  &nbsp;</span>
+    <span aria-hidden="true" class="front-text">&nbsp;PROMAC&nbsp;</span>
+</button>
+</footer>
