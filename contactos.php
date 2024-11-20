39006f88587c58c460ac0f18eea79d4589b2e543
Jesus Almanza <crhistiko@gmail.com>
2024-11-20 11:49:23 -0400

    Avance 90% proyecto final 11/20/2024

--- contactos.php
+++ contactos.php
@@ -10,29 +10,6 @@
 </head>
 <body>
 <?php require_once "header.php"; ?>
-<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active" data-bs-interval="10000">
-            <img src="img/Productos/Trabajos%20echos/Contacto/PERRETOS.png" class="d-block w-100 " alt="logo">
-          </div>
-          <div class="carousel-item" data-bs-interval="2000">
-            <img src="img/Productos/Trabajos%20echos/Contacto/PERRETOS.png" class="d-block w-100" alt="img">
-          </div>
-          <div class="carousel-item">
-            <img src="img/Productos/Trabajos%20echos/Contacto/gatetos.png" class="d-block w-100" alt="portada">
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
-
-      
 <div class="form-card1">
   <div class="form-card2">
     <form class="form">
@@ -74,6 +51,33 @@
     </form>
   </div>
 </div>
+
+
+
+
+<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active" data-bs-interval="10000">
+            <img src="img/Productos/Trabajos%20echos/Contacto/PERRETOS.png" class="d-block w-100 " alt="logo">
+          </div>
+          <div class="carousel-item" data-bs-interval="2000">
+            <img src="img/Productos/Trabajos%20echos/Contacto/contacto_ojos.png" class="d-block w-100" alt="img">
+          </div>
+          <div class="carousel-item">
+            <img src="img/Productos/Trabajos%20echos/Contacto/gatetos.png" class="d-block w-100" alt="portada">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+      
 
 
     
